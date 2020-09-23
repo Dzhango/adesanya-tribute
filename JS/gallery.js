4fbc0cf4860f7418b7d81b69b5414b54c9070ca2
dbayandarov@gmail.com <dbayandarov@gmail.com>
2020-09-23 12:42:39 -0700

    Added images to the gallery

--- JS/gallery.js
+++ JS/gallery.js
@@ -1,30 +1,29 @@
 
 //images array global, images are created dynamically
-const images = ["knee", "dance", "awards", "face-off", "champion", "flag"];
-const imgs = document.querySelector(".imgs");
-const mainImg = document.querySelector(".img-main");
+// const images = ["knee", "dance", "awards", "face-off", "champion", "flag"];
+const imgArr = document.querySelectorAll(".imgs img");
+const current = document.querySelector(".img-main img");
 
+// function displayImg(arr){
+//     // main img
+//     let img = document.createElement("img");
+//     img.src = `../media/gallery/${arr[0]}.jpg`;
+//     img.id = "current";
+//     mainImg.appendChild(img);  
 
-function displayImg(arr){
-    // main img
-    let img = document.createElement("img");
-    img.src = `../media/gallery/${arr[0]}.jpg`;
-    img.id = "current";
-    mainImg.appendChild(img);  
-
-    //img gallery
-    for (let i = 0; i < arr.length; i++){ 
-        img = document.createElement("img");
-        img.src = `../media/gallery/${arr[i]}.jpg`
-        imgs.appendChild(img);
-    }
-}
+//     //img gallery
+//     for (let i = 0; i < arr.length; i++){ 
+//         img = document.createElement("img");
+//         img.src = `../media/gallery/${arr[i]}.jpg`
+//         imgs.appendChild(img);
+//     }
+// }
 
 function imgClick(el){
     const opacity = 0.3;
     imgArr.forEach(image => (image.style.opacity = 1));
 
-    current.src = el.target.src; 
+    current.src = el.target.src;    
 
     current.classList.add("fade-in");
 
@@ -32,12 +31,8 @@ function imgClick(el){
     setTimeout(() => current.classList.remove("fade-in"), 500);
 
     el.target.style.opacity = opacity;
-    
 }
 
-displayImg(images); 
-
-const imgArr = document.querySelectorAll(".imgs img");
 imgArr.forEach(element => {
     element.addEventListener('click', imgClick);
 });
